6ab824e5aa4437738d09b2191523afcf21d49dd6
spfx15 <A250862IK@rvt.lv>
2026-02-24 11:15:30 +0200

    Add files via upload

---
+++ uzdevums4.py
@@ -0,0 +1,20 @@
+# Hostname; IP; Latency(ms); Status
+nodes = [
+"Srv-Web-01;192.168.1.10;15;UP",
+"Srv-DB-01;192.168.1.20;450;UP",
+"Srv-Backup;10.0.0.5;0;DOWN",
+"Workstation-A;192.168.1.105;5;UP",
+"Srv-Proxy-01;172.16.0.1;10;up",
+"Srv-Mail;10.0.0.10;120;UP",
+"Router-Core;192.168.1.1;2;UP",
+"Srv-Dev-01;192.168.2.50;500;UP",
+"Printer-Main;192.168.1.200;0;down",
+"Srv-Log;10.0.0.15;105;UP"
+]
+ip_part = input("Ievadi IP adreses daļu:")
+print("Serveri šaja tikla:")
+for node in nodes:
+    hostname = node.split(";")[0]
+    ip = node.split(";")[1]
+    if ip.startswith(ip_part):
+        print(hostname)
